f8f77b5df843ea2a850c5c409e313b611f9661bd
frankqianms <frankqian@microsoft.com>
2022-11-03 16:59:50 +0800

    chore: add me-we-dashboard sample

---
+++ me-we-dashboard/tabs/src/service/login.ts
@@ -0,0 +1,18 @@
+import { FxContext } from "../components/singletonContext";
+
+export const scope = [
+  "Files.ReadWrite.All",
+  "Tasks.ReadWrite",
+  "Calendars.ReadWrite"
+];
+
+export function loginAction(scope: string[]) {
+  try {
+    var teamsfx = FxContext.getInstance().getTeamsFx();
+    teamsfx.login(scope);
+    FxContext.getInstance().setTeamsFx(teamsfx);
+  } catch (e) {
+    console.log(e);
+    throw "Login Error: can not login!";
+  }
+}
